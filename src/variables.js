bf4f071279e376b21bc1a539302b438ed1e251c6
BARTOSNicolas <73882719+BARTOSNicolas@users.noreply.github.com>
2025-10-15 13:53:47 +0200

    Add Variable

---
+++ src/variables.js
@@ -0,0 +1,14 @@
+const myString = "Non, je mets des caleçons"
+const myNumber = 7
+let myBoolean = true
+
+let myArray = ["Gateau","Ananas","Pepsi"]
+let myObject = {
+    name: "Nicolas",
+    age: 45
+}
+console.log(myString)
+console.log(myNumber)
+console.log(myBoolean)
+console.log(myArray)
+console.log(myObject)
